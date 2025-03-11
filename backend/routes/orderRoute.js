e5619de78428b73b5e2479e540287f0f9a63767b
sharath <sharath.t9480@gmail.com>
2025-03-11 19:37:43 +0530

    added cashfree

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,22 +1,15 @@
 import express from "express"
 import authMiddleware from "../middleware/auth.js";
 
-<<<<<<< HEAD
 import { placeOrder, verifyOrder,userOrders } from "../controllers/orderContoller.js";
-=======
-import { placeOrder } from "../controllers/orderContoller.js";
->>>>>>> 9318b2611041fc2619d913f59a391ae474bb7dfa
 
 
 const orderRouter=express.Router();
 
 orderRouter.post("/place",authMiddleware,placeOrder);
 
-<<<<<<< HEAD
 orderRouter.post("/verify",verifyOrder);
 orderRouter.post("/userorders",authMiddleware,userOrders)
-=======
->>>>>>> 9318b2611041fc2619d913f59a391ae474bb7dfa
 export default orderRouter;
 
 
